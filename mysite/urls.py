e881cbe6885c20df1336a7068f411341d5f5541f
unknown <esthercarolinaguzmandeleon@gmail.com>
2023-01-09 11:06:19 +0100

    login

--- mysite/urls.py
+++ mysite/urls.py
@@ -27,6 +27,7 @@ sitemaps = {
 }
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path("admin/", admin.site.urls),
     path("", include("blog.urls"), name="blog-urls"),
     path("summernote/", include("django_summernote.urls")),
